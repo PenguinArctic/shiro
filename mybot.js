265a410216fe7ff6c41092b204d6e9e540011222
rikumax25 <jorgev259@outlook.es>
2017-11-16 15:11:55 -0600

    cleaned up mybot.js

--- mybot.js
+++ mybot.js
@@ -24,52 +24,35 @@ client.on('message', message => {
 
     if (!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {}
     if (!userData[sender.id + message.guild.id].lastDaily) userData[sender.id + message.guild.id].lastDaily = "Not Collected";
-        fs.writeFile("Storage/userData.json", JSON.stringify(userData), (err) => {
-           if (err) console.error(err)         
-     });
+    fs.writeFile("Storage/userData.json", JSON.stringify(userData), (err) => {
+        if (err) console.error(err)
+    });
 
- //------------------------------------------------------------
-    
-     if (msg.startsWith(`${prefix}BUY`)) { 
+    //------------------------------------------------------------
 
-         let categories = [];
-
-         if (!args.join(" ")) {
-
+    if (msg.startsWith(`${prefix}BUY`)) {
+        let categories = [];
+        if (!args.join(" ")) {
             for (var i in items) {
-
                 if (!categories.includes(items[i].type)) {
                     categories.push(items[i].type)
                 }
-
             }
 
             const embed = new Discord.RichEmbed()
-                .setDescription(`Available Items`)
-                .setColor(0xD4AF37)
+            .setDescription(`Available Items`)
+            .setColor(0xD4AF37)
 
             for (var i = 0; i < categories.length; i++) {
-
                 var tempDesc = '';
-
                 for (var c in items) { 
                     if (categories[i] === items[c].type) {
-
                         tempDesc += `${items[c].name} - $${items[c].price} - ${items[c].desc}\n`;
-
                     }
-
                 }
-
                 embed.addField(categories[i], tempDesc);
-
             }
-
-            return message.channel.send({
-                embed
-            });
-
-
+            return message.channel.send({embed});
         }
 
 
@@ -87,225 +70,184 @@ client.on('message', message => {
 
         if (itemName === '') {
             return message.channel.send({embed: {
+                color: 10181046,
+                author: {
+                    name: message.author.username,
+                    icon_url: message.author.avatarURL
+                },
 
-    color: 10181046,
-    author: {
-      name: message.author.username,
-      icon_url: message.author.avatarURL
-    },
+                fields: [{
+                    name: "Shop",
+                    value: `**Item ${args.join(" ").trim()} not found.**`
+                }]
+            }})
 
-    fields: [{
-        name: "Shop",
-        value: `**Item ${args.join(" ").trim()} not found.**`
-  }]
-}})
+            economy.fetchBalance(message.author.id + message.guild.id).then((i) => {
+                if (i.money <= itemPrice) {
+                    return message.channel.send({embed: {
+                        color: 10181046,
+                        author: {
+                            name: message.author.username,
+                            icon_url: message.author.avatarURL
+                        },
 
-    economy.fetchBalance(message.author.id + message.guild.id).then((i) => {
-            if (i.money <= itemPrice) { 
+                        fields: [{
+                            name: "Shop",
+                            value: `**You don't have enough money for this item.**`
+                        }]
+                    }});
+                }
 
-                return message.channel.send({embed: {
+                economy.updateBalance(message.author.id + message.guild.id, parseInt(`-${itemPrice}`)).then((i) => {
+                    message.channel.send('**You bought ' + itemName + '!**');
 
-    color: 10181046,
-    author: {
-      name: message.author.username,
-      icon_url: message.author.avatarURL
-    },
-
-    fields: [{
-        name: "Shop",
-        value: `**You don't have enough money for this item.**`
-  }]
-}});
-
-
+                    if (itemName === 'Nichijou Pack') {
+                        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫Nichijou"));
+                    }
+                    if (itemName === 'Maid Dragon Pack') {
+                        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫Maid Dragon"));
+                    }
+                    if (itemName === 'LWA Pack') {
+                        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫LWA"));
+                    }
+                    if (itemName === 'JOJO Pack') {
+                        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫JOJO"));
+                    }
+                    if (itemName === 'Noragami Pack') {
+                        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫Noragami"));
+                    }
+                    if (itemName === 'Gabriel dropout') {
+                        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫Gabriel dropout"));
+                    }
+                    if (itemName === 'Monogatari Pack') {
+                        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫Monogatari"));
+                    }
+                })
+            })
+        }
+        //------------------------------------------------------------
+        if (msg.startsWith(`${prefix}ADDMONEY`)) {
+            if (!message.member.roles.find("name", modRole)) {
+                message.channel.send({embed: {
+                    color: 10181046,
+                    author: {
+                        name: message.author.username,
+                        icon_url: message.author.avatarURL
+                    },
+                    fields: [{
+                        name: "Add Money",
+                        value: '**You need the role `' + modRole + '` to use this command...**'
+                    }]
+                }})
+                return;
             }
 
-
-            economy.updateBalance(message.author.id + message.guild.id, parseInt(`-${itemPrice}`)).then((i) => {
-
-                message.channel.send('**You bought ' + itemName + '!**');
-
-                if (itemName === 'Nichijou Pack') {
-                    message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫Nichijou"));
-                }
-                if (itemName === 'Maid Dragon Pack') {
-                    message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫Maid Dragon"));
-                }
-                if (itemName === 'LWA Pack') {
-                    message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫LWA"));
-                }
-                if (itemName === 'JOJO Pack') {
-                    message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫JOJO"));
-                }
-                if (itemName === 'Noragami Pack') {
-                    message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫Noragami"));
-                }
-                if (itemName === 'Gabriel dropout') {
-                    message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫Gabriel dropout"));
-                }
-                if (itemName === 'Monogatari Pack') {
-                    message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "🎫Monogatari"));
-                }
-            })
-
-        })
-
-    }
- //------------------------------------------------------------
-    if (msg.startsWith(`${prefix}ADDMONEY`)) {
-
- 
-        if (!message.member.roles.find("name", modRole)) { 
-            message.channel.send({embed: {
-
-    color: 10181046,
-    author: {
-      name: message.author.username,
-      icon_url: message.author.avatarURL
-    },
-
-    fields: [{
-        name: "Add Money",
-        value: '**You need the role `' + modRole + '` to use this command...**'
-  }]
-}})
-
-
-            return;
-        }
-
-        if (!args[0]) {
-            message.channel.send({embed: {
-
-    color: 10181046,
-    author: {
-      name: message.author.username,
-      icon_url: message.author.avatarURL
-    },
-
-    fields: [{
-        name: "Add Money",
-        value: `**You need to define an amount. Usage: ${prefix}BALSET <amount> <user>**`
-  }]
-}})
-
-
-
-
-
-
-            return;
-        }
-
-        if (isNaN(args[0])) {
-            message.channel.send({embed: {
-
-    color: 10181046,
-    author: {
-      name: message.author.username,
-      icon_url: message.author.avatarURL
-    },
-
-    fields: [{
-        name: "Add Money",
-        value: `**The amount has to be a number. Usage: ${prefix}BALSET <amount> <user>**`
-  }]
-}}
-
-                    );
-            return; 
-        }
-
-        let defineduser = '';
-        if (!args[1]) { 
-            defineduser = message.author.id;
-        } else { 
-            let firstMentioned = message.mentions.users.first();
-            defineduser = firstMentioned.id;
-        }
-
- 
-        economy.updateBalance(defineduser + message.guild.id, parseInt(args[0])).then((i) => { 
-            message.channel.send(`**User defined had ${args[0]} added/subtraction from their account.**`)
-        });
-}
-//------------------------------------------------------------
-
-    if (msg === `${prefix}BALANCE` || msg === `${prefix}MONEY`) { 
-
-       
-        economy.fetchBalance(message.author.id + message.guild.id).then((i) => {
-            
-                 
-            message.channel.send({embed: {
-
-    color: 10181046,
-    author: {
-      name: message.author.username,
-      icon_url: message.author.avatarURL
-    },
-
-    fields: [{
-        name: "Money",
-        value: `**Account Balance:** ${i.money}💴`
-  }]
-}})         
-
-        })
-
-    }
-//------------------------------------------------------------
-
-if (message.content.toUpperCase() === `${prefix}DAILY`) {
-  if (userData[sender.id + message.guild.id].lastDaily != moment().format(`L`)){
-    userData[sender.id + message.guild.id].lastDaily = moment().format(`L`)          
-    
-    mainuser = message.author.id;
-    
-            economy.updateBalance(mainuser + message.guild.id, 500).then((i) => { 
+            if (!args[0]) {
                 message.channel.send({embed: {
+                    color: 10181046,
+                    author: {
+                        name: message.author.username,
+                        icon_url: message.author.avatarURL
+                    },
 
-    color: 10181046,
-    author: {
-      name: message.author.username,
-      icon_url: message.author.avatarURL
-    },
+                    fields: [{
+                        name: "Add Money",
+                        value: `**You need to define an amount. Usage: ${prefix}BALSET <amount> <user>**`
+                    }]
+                }})
+                return;
+            }
 
-    fields: [{
-        name: "Daily collection",
-        value: `**You got $500! New Balance:** ${i.money}`
-  }]
-}})
-                
-                
-                
-                
-                
-    fs.writeFile("Storage/userData.json", JSON.stringify(userData), (err) => {
-        if (err) console.error(err)         
-})
-            })} else {
+            if (isNaN(args[0])) {
+                message.channel.send({embed: {
+                    color: 10181046,
+                    author: {
+                        name: message.author.username,
+                        icon_url: message.author.avatarURL
+                    },
 
-                
-                
-message.channel.send({embed: {
+                    fields: [{
+                        name: "Add Money",
+                        value: `**The amount has to be a number. Usage: ${prefix}BALSET <amount> <user>**`
+                    }]
+                }});
+                return;
+            }
 
-    color: 10181046,
-    author: {
-      name: message.author.username,
-      icon_url: message.author.avatarURL
-    },
+            let defineduser = '';
+            if (!args[1]) {
+                defineduser = message.author.id;
+            } else {
+                let firstMentioned = message.mentions.users.first();
+                defineduser = firstMentioned.id;
+            }
 
-    fields: [{
-        name: "Daily collection",
-        value: `**You already collected your daily reward! You can collect your next reward**` + moment().endOf(`**day**`).fromNow() + `.`
- }]
+            economy.updateBalance(defineduser + message.guild.id, parseInt(args[0])).then((i) => {
+                message.channel.send(`**User defined had ${args[0]} added/subtraction from their account.**`)
+            });
+        }
+        //------------------------------------------------------------
+
+        if (msg === `${prefix}BALANCE` || msg === `${prefix}MONEY`) {
+            economy.fetchBalance(message.author.id + message.guild.id).then((i) => {
+                message.channel.send({embed: {
+                    color: 10181046,
+                    author: {
+                        name: message.author.username,
+                        icon_url: message.author.avatarURL
+                    },
+
+                    fields: [{
+                        name: "Money",
+                        value: `**Account Balance:** ${i.money}💴`
+                    }]
+                }})
+
+            })
+        }
+        //------------------------------------------------------------
+
+        if (message.content.toUpperCase() === `${prefix}DAILY`) {
+            if (userData[sender.id + message.guild.id].lastDaily != moment().format(`L`)){
+                userData[sender.id + message.guild.id].lastDaily = moment().format(`L`)
+
+                mainuser = message.author.id;
+
+                economy.updateBalance(mainuser + message.guild.id, 500).then((i) => {
+                    message.channel.send({embed: {
+                        color: 10181046,
+                        author: {
+                            name: message.author.username,
+                            icon_url: message.author.avatarURL
+                        },
+
+                        fields: [{
+                            name: "Daily collection",
+                            value: `**You got $500! New Balance:** ${i.money}`
+                        }]
+                    }})
+
+                    fs.writeFile("Storage/userData.json", JSON.stringify(userData), (err) => {
+                        if (err) console.error(err)
+                    })
+                })
+            }else{
+                message.channel.send({embed: {
+                    color: 10181046,
+                    author: {
+                        name: message.author.username,
+                        icon_url: message.author.avatarURL
+                    },
+
+                    fields: [{
+                        name: "Daily collection",
+                        value: `**You already collected your daily reward! You can collect your next reward**` + moment().endOf(`**day**`).fromNow() + `.`
+                    }]}
+                })
+            }
+        }
     }
-       })
-           }}
-
+})
 //------------------------------------------------------------
-      }})
 
-  
 client.login(config.token)
