993a9f5c768fab50efa51aec0c37d0aebd6bf439
rikumax25 <jorgev259@outlook.es>
2017-11-30 10:56:18 -0600

    complete overhaul

--- mybot.js
+++ mybot.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
-const economy = require('discord-eco-chito');
-economy.start("./userData.sqlite");
 
 const fs = require('fs'); 
 const moment = require('moment');
 var util = require("../akira/utilities.js")
 var config = require("./Storage/config.json");
 
+var profile = require("../data/exp.json");
 let items = JSON.parse(fs.readFileSync('Storage/items.json', 'utf8'));
 var inventory = JSON.parse(fs.readFileSync('../data/inventory.json', 'utf8'));
 
@@ -25,7 +24,9 @@ client.on('message', message => {
 	//------------------------------------------------------------
 
 	authoruser = message.author.id;
-	economy.updateBalance(authoruser + message.guild.id, Math.floor(Math.random() * 3));
+	profile[authoruser].money += Math.floor(Math.random() * 3);
+	util.save(profile,"exp");
+
 	if(inventory[authoruser] == undefined) {
 		inventory[authoruser]={};
 		util.save(inventory,"inventory");
@@ -71,14 +72,6 @@ client.on('message', message => {
 				}
 			}
 
-			if(args[0]=="Background"){
-				itemName = items["Background"].name;
-				itemPrice = items["Background"].price;
-				itemDesc = items["Background"].desc;
-				itemType = items["Background"].type;
-				item = items["Background"]
-			}
-
 			if (itemName === '') {
 				return message.channel.send({embed: {
 					color: 10181046,
@@ -94,62 +87,63 @@ client.on('message', message => {
 				}})
 			}
 
-			economy.fetchBalance(message.author.id + message.guild.id).then((i) => {
-				if (i.money <= itemPrice) {
-					return message.channel.send({embed: {
-						color: 10181046,
-						author: {
-							name: message.author.username,
-							icon_url: message.author.avatarURL
-						},
+			var user = profile[message.author.id];
+			if (user.money < itemPrice) {
+				return message.channel.send({embed: {
+					color: 10181046,
+					author: {
+						name: message.author.username,
+						icon_url: message.author.avatarURL
+					},
 
-						fields: [{
-							name: "Shop",
-							value: `**You don't have enough money for this item.**`
-						}]
-					}});
-				}
+					fields: [{
+						name: "Shop",
+						value: `**You don't have enough money for this item.**`
+					}]
+				}});
+			}
 
-				switch(itemType){
-					case "Packs":
-						economy.updateBalance(message.author.id + message.guild.id, parseInt(`-${itemPrice}`))
-						message.member.addRole(message.guild.roles.find("name", item.role));
-						message.channel.send('**You bought ' + itemName + '!**');
-						message.guild.channels.find("name",item.channel).send(`<@${message.author.id}>`).then(m=>m.delete("New channel ping"))
-						break;
+			switch(itemType){
+				case "Packs":
+					profile[message.author.id].money += -itemPrice;
+					message.member.addRole(message.guild.roles.find("name", item.role));
+					message.channel.send('**You bought ' + itemName + '!**');
+					message.guild.channels.find("name",item.channel).send(`<@${message.author.id}>`).then(m=>m.delete("New channel ping"))
+					break;
 
-					case "Utilities":
-						switch(itemName){
-							case "Nickname Change":
-								try{economy.updateBalance(message.author.id + message.guild.id, parseInt(`-${itemPrice}`));}catch(e){console.log(e)}
-								message.member.addRole(message.guild.roles.find("name", item.role));
-								message.channel.send('**You bought ' + itemName + '!**');
-								message.guild.channels.find("name",item.channel).send(`<@${message.author.id}>`).then(m=>m.delete("New channel ping"))
-								break;
+				case "Utilities":
+					switch(itemName){
+						case "Nickname Change":
+							profile[message.author.id].money += -itemPrice;
+							message.member.addRole(message.guild.roles.find("name", item.role));
+							message.channel.send('**You bought ' + itemName + '!**');
+							message.guild.channels.find("name",item.channel).send(`<@${message.author.id}>`).then(m=>m.delete("New channel ping"))
+							util.save(profile,"exp");
+							break;
 
-							case "Background":
-								message.channel.send("Write the number of the desired background (You can see them here https://www.fandomcircle.com/shop-1#PROFILES)").then(proposal => {
-									const collector = message.channel.createMessageCollector(
-										m => m.author.id == message.author.id,
-										{ max: 1 }
-									);
-									collector.on('collect', m => {
-										var number = parseInt(m.content.split(" ")[0]);
-										if(inventory[m.author.id][`bg${number}`]){
-											m.reply("You already have this background. Set it using >background <number>")
-										}else{
-											economy.updateBalance(m.author.id + m.guild.id, parseInt(`-${itemPrice}`));
-											inventory[m.author.id][`bg${number}`] = true;
-											m.reply("Thanks for buying this background ^.^. Set it using >background <number>");
-											util.save(inventory,"inventory");
-										}
-									})
+						case "Background":
+							message.channel.send("Write the number of the desired background (You can see them here https://www.fandomcircle.com/shop-1#PROFILES)").then(proposal => {
+								const collector = message.channel.createMessageCollector(
+									m => m.author.id == message.author.id,
+									{ max: 1 }
+								);
+								collector.on('collect', m => {
+									var number = parseInt(m.content.split(" ")[0]);
+									if(inventory[m.author.id][`bg${number}`]){
+										m.reply("You already have this background. Set it using >background <number>")
+									}else{
+										profile[m.author.id].money += -itemPrice;
+										inventory[m.author.id][`bg${number}`] = true;
+										m.reply("Thanks for buying this background ^.^. Set it using >background <number>");
+										util.save(inventory,"inventory");
+										util.save(profile,"exp");
+									}
 								})
-								break;
-						}
-						break;
-				}
-			})
+							})
+							break;
+					}
+					break;
+			}
 		}
 	}
 	//------------------------------------------------------------
@@ -209,14 +203,35 @@ client.on('message', message => {
 			defineduser = firstMentioned.id;
 		}
 
-		economy.updateBalance(defineduser + message.guild.id, parseInt(args[0])).then((i) => {
-			message.channel.send(`**User defined had ${args[0]} added/subtraction from their account.**`)
-		});
+		profile[defineduser].money += parseInt(args[0])
+		util.save(profile,"exp");
+		message.channel.send(`**User defined had ${args[0]} added/subtraction from their account.**`)
 	}
 	//------------------------------------------------------------
 
 	if (msg === `${prefix}BALANCE` || msg === `${prefix}MONEY`) {
-		economy.fetchBalance(message.author.id + message.guild.id).then((i) => {
+		message.channel.send({embed: {
+			color: 10181046,
+			author: {
+				name: message.author.username,
+				icon_url: message.author.avatarURL
+			},
+
+			fields: [{
+				name: "Money",
+				value: `**Account Balance:** ${profile[message.author.id].money}💴`
+			}]
+		}})
+
+	}
+	//------------------------------------------------------------
+
+	if (message.content.toUpperCase() === `${prefix}DAILY`) {
+		if(profile[message.author.id].lastDaily == "Not Collected" || moment.duration(moment().diff(moment(profile[message.author.id].lastDaily,"YYYY-MM-DD kk:mm"))).asHours() >= 24){
+			profile[message.author.id].lastDaily = moment().format("YYYY-MM-DD kk:mm");
+			profile[message.author.id].money += 500;
+			util.save(profile,"exp");
+
 			message.channel.send({embed: {
 				color: 10181046,
 				author: {
@@ -225,50 +240,24 @@ client.on('message', message => {
 				},
 
 				fields: [{
-					name: "Money",
-					value: `**Account Balance:** ${i.money}💴`
+					name: "Daily collection",
+					value: `**You got $500! New Balance:** ${profile[message.author.id].money}`
 				}]
 			}})
+		}else{
+			message.channel.send({embed: {
+				color: 10181046,
+				author: {
+					name: message.author.username,
+					icon_url: message.author.avatarURL
+				},
 
-		})
-	}
-	//------------------------------------------------------------
-
-	if (message.content.toUpperCase() === `${prefix}DAILY`) {
-		economy.fetchBalance(message.author.id + message.guild.id).then((i) => {
-			console.log(i.lastDaily);
-			if(i.lastDaily == "Not Collected" || moment.duration(moment().diff(moment(i.lastDaily,"YYYY-MM-DD kk:mm"))).asHours() >= 24){
-				economy.updateLastDaily(message.author.id + message.guild.id,moment().format("YYYY-MM-DD kk:mm"))
-
-				economy.updateBalance(message.author.id + message.guild.id, 500).then((i) => {
-					message.channel.send({embed: {
-						color: 10181046,
-						author: {
-							name: message.author.username,
-							icon_url: message.author.avatarURL
-						},
-
-						fields: [{
-							name: "Daily collection",
-							value: `**You got $500! New Balance:** ${i.money}`
-						}]
-					}})
-				})
-			}else{
-				message.channel.send({embed: {
-					color: 10181046,
-					author: {
-						name: message.author.username,
-						icon_url: message.author.avatarURL
-					},
-
-					fields: [{
-						name: "Daily collection",
-						value: `**You already collected your daily reward! You can collect your next reward** in ${24 - Math.floor(moment.duration(moment().diff(moment(i.lastDaily,"YYYY-MM-DD kk:mm"))).asHours())} hours.`
-					}]}
-									 })
-			}
-		})
+				fields: [{
+					name: "Daily collection",
+					value: `**You already collected your daily reward! You can collect your next reward** in ${24 - Math.floor(moment.duration(moment().diff(moment(profile[message.author.id].lastDaily,"YYYY-MM-DD kk:mm"))).asHours())} hours.`
+				}]}
+								 })
+		}
 
 	}
 	//--------------------------------------------------------------------
