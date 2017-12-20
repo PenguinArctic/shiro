8723603fd6e6b5f460de2c1fc2cbf37ac1a91754
rikumax25 <jorgev259@outlook.es>
2017-12-20 08:42:07 -0600

    add akira permissions

--- mybot.js
+++ mybot.js
@@ -63,24 +63,24 @@ client.on('message', message => {
      //------------------ MONEY GIVEN PER MESSAGE ----------
 		var profile = json.readFileSync('../data/exp.json');
 		profile[message.author.id].money += Math.floor(Math.random() * 3);
-		util.save(profile,"exp");       
+		util.save(profile,"exp");
+		
+		if(message.content.startsWith(prefix) || message.content.startsWith("<@!" + client.user.id + ">")){
+			var param = message.content.split(" ");
+		
+			if(message.content.startsWith(prefix)){
+				param[0] = param[0].split(prefix)[1];
+			}else{
+				param.splice(0,1);
+			}
+
+			if(util.permCheck(message,param[0].toLowerCase())){
+				if (!client.commands.has(param[0].toLowerCase())) return;				
+				client.commands.get(param[0].toLowerCase()).execute(client, message, args);
+			}
+		}
 
         switch(message.channel.name){
-			case "shop": //ONLY WORKS IN THE SHOP
-				if(message.content.startsWith(prefix) || message.content.startsWith("<@!" + client.user.id + ">")){
-					var param = message.content.split(" ");
-				
-					if(message.content.startsWith(prefix)){
-						param[0] = param[0].split(prefix)[1];
-					}else{
-						param.splice(0,1);
-					}
-
-					if (!client.commands.has(param[0].toLowerCase())) return;				
-					client.commands.get(param[0].toLowerCase()).execute(client, message, args);
-				}
-				break;
-
 			case "shiro":
 				util.talk(client,message);  //BMO CAN TALK O.O
 				break;
